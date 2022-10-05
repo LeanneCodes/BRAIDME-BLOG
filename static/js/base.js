b1a8ebb25bc2727e3c5987c165500e3ba69ecdb2
Leanne Goldsmith <leannegoldsmith95@yahoo.com>
2022-10-05 19:28:47 +0000

    homepage css connected

---
+++ static/js/base.js
@@ -0,0 +1,16 @@
+function changeTopnav() {
+    var navbar = document.getElementById('navbar-bm');
+    var scrollvalue = window.scrollY;
+    console.log(scrollvalue);
+    if(scrollvalue < 30) {
+        navbar.classList.remove('topnav-color');
+    } else {
+        navbar.classList.add('topnav-color');
+    }
+}
+
+window.addEventListener('scroll', changeTopnav);
+
+setTimeout(() => {
+    $('.alert').alert('close')
+}, 5000)
