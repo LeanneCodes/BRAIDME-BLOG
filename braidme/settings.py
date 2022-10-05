5b5d08b72ce0893fef92752b408463430ea977f5
Leanne Goldsmith <leannegoldsmith95@yahoo.com>
2022-10-05 20:26:16 +0000

    add the blog app

--- braidme/settings.py
+++ braidme/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+from django.contrib.messages import constants as messages
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n)b4&6tme#26biq=((58vzuje#t+96$(mkj%80p8^qf6+&r=3+'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,11 +43,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'cloudinary_storage',
+    'cloudinary',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
     'home',
     'products',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -103,11 +110,15 @@ WSGI_APPLICATION = 'braidme.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://jqzhykstheyslo:2ec9aadc1ab14099921cd8589700388e98765322f776f44cb0ed425a9b086eb3@ec2-34-247-72-29.eu-west-1.compute.amazonaws.com:5432/dbm9f41hfcmf5t"
+os.environ["SECRET_KEY"] = "Braidme2022!"
