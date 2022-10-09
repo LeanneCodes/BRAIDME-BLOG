37cf805e22f9b09fe0d3a3204bd8304eb3a9c599
Leanne Goldsmith <leannegoldsmith95@yahoo.com>
2022-10-09 15:57:00 +0000

    amend css for blog management

--- posts/forms.py
+++ posts/forms.py
@@ -21,4 +21,4 @@ class PostForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
 
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black rounded-0'
+            field.widget.attrs['class'] = 'border-grey'
